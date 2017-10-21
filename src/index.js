20daa039bfd2106056b383059b0073caa8a2d172
Umpoy <iumpoy@yahoo.com>
2017-10-21 12:32:45 -0700

    react-intro -Audrian Manly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.ceil(Math.random() * 1000);
+
+const greeting = (u) =>{
+	let response = (
+	<div className="container">
+		<h1>Hello, {u.name}</h1>
+		<h2 className="text-muted">Your lucky number is: {u.luckyNumber}</h2>
+	</div>
+	);
+	return response
+} 
+
+const user = {
+	name: 'TM87 Master',
+	luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
+
     document.getElementById('root')
 );
